c9ff74fb2cf90a346fbbea529958b255d917c0bf
achujoseph0709 <achujozefsl0709@gmail.com>
2023-08-08 16:06:55 +0530

    print

---
+++ print.py
@@ -0,0 +1,7 @@
+print('Achu\'s "LapTop"' *10) #\ to avoid the the ' "
+
+# \n means New Line and to avoid that we can put a r as the intial string
+
+print (r'c:\achu\newsLetter')
+
+#The print() function can be used for various purposes, including debugging, user interaction, and outputting information to the console
